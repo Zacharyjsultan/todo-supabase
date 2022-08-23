6418e8da27b803141785defe3c1a4dbc3fadfa74
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-23 13:57:19 -0700

    render function

--- render-utils.js
+++ render-utils.js
@@ -3,16 +3,20 @@ export function renderTodo(todo, handleComplete) {
 
     const div = document.createElement('div');
     const p = document.createElement('p');
+
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
     div.classList.add(todo.complete ? 'complete' : 'incomplete');
 
-    // add the 'todo' css class no matter what
-
+    // add the 'todo' css class no matter what lol
+    div.classList.add(todo);
     // put the todo's text into the p tag
-
+    p.textContent = todo.todo;
     // append stuff
-
+    div.append(p);
     // add event listener for click and call handleComplete function
-    
+    div.addEventListener('click', () => {
+
+    });
     // return the div
+    return div;
 }
